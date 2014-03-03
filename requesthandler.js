f3a280062fd3a844ce0a496c9d6c5fc0816015ee
natemartin519 <natemartin519@gmail.com>
2014-03-03 10:28:43 -0500

    POST routing

--- requesthandler.js
+++ requesthandler.js
@@ -1,19 +1,31 @@
-var exec = require("child_process").exec;
+var querystring = require('querystring');
 
 function start(response) {
     console.log('Request handler \'start\' wes called');
 
-    exec("ls -lah", function (error, stdout, stderr) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
-        response.end();
-    });
+    var body = '<!doctype html>' +
+        '<html lang="en">' +
+        '<head>' +
+        '<meta charset="UTF-8">' +
+        '<title>My First Node Page</title>' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text">'+
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(body);
+    response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log('Request handler \'upload\' wes called');
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Updoad");
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('You have sent: ' + querystring.parse(postData).text);
     response.end();
 }
 
